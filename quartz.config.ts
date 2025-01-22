aa6b73cf383a2a6bf27b901bf2bd6155d65c7fba
Mindaugas Vaitkevičius <min.vaitkevicius@gmail.com>
2025-01-22 10:11:54 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "minvaitkevicius.github.io/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
